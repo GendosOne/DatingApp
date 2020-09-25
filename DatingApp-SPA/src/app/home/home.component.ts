58c85e340232e693f1fa187e4b9284310761970d
Gennadij Goleniščev <gennadijgoleniscev@gmail.com>
2020-09-25 14:34:59 +0200

    End of section 4

---
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { timingSafeEqual } from 'crypto';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor(private http: HttpClient) { }
+// tslint:disable-next-line: typedef
+ngOnInit() {
+  }
+
+// tslint:disable-next-line: typedef
+  registerToggle(){
+    this.registerMode = true;
+  }
+
+
+  cancelRegisterMode(registerMode: boolean){
+    this.registerMode = registerMode;
+  }
+}
