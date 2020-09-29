7f6cd473c57cc82c1e787a37686e2b59d2439d25
Gennadij Goleniščev <gennadijgoleniscev@gmail.com>
2020-09-29 17:22:52 +0200

    End of section 7

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Route } from '@angular/compiler/src/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { timingSafeEqual } from 'crypto';
+import { AlertifyService } from '../_services/alertify.service';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertify: AlertifyService
+  ){}
+  canActivate(): boolean {
+    if(this.authService.loggedIn()){
+      return true;
+    }
+
+    this.alertify.error('You shall not pass!!!');
+    this.router.navigate(['/home']);
+  }
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,6 +12,11 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { approutes } from './routes';
+
 
 @NgModule({
   declarations: [
@@ -18,14 +24,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       NavComponent,
       HomeComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
   imports: [
     BrowserModule,
    HttpClientModule,
    FormsModule,
    BrowserAnimationsModule,
-   BsDropdownModule.forRoot()
+   BsDropdownModule.forRoot(),
+   RouterModule.forRoot(approutes)
   ],
   providers: [
     AuthService,
